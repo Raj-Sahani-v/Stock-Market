85470aadc1b8dbe91692c1d5b7502361d86a2f80
Raj Sahani <rajsahani85048504@gmail.com>
2026-07-10 16:05:28 +0530

    nav error fix

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -3,36 +3,39 @@ import { Link } from "react-router"
 
 export const NavBar = () => {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom">
-  <div class="container-fluid">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary border-bottom">
+  <div className="container-fluid  ">
 
     
-    <Link class="navbar-brand d-flex align-items-center ps-lg-5 " to="/">
-      <img src="images/newlogo.jpeg" alt="brand logo" style={{width:"15%",}} class="me-2"/>
+    <Link className="navbar-brand d-flex align-items-center ps-lg-5 " to="/">
+      <img src="images/newlogo.jpeg" alt="brand logo" style={{width:"auto",height:"40px"}} class="me-2"/>
     </Link>
 
-    <button class="navbar-toggler" type="button"
+    <button className="navbar-toggler  " type="button"
       data-bs-toggle="collapse"
       data-bs-target="#navbarSupportedContent">
-      <span class="navbar-toggler-icon"></span>
+      <span className="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <Link class="nav-link active" to="/signup">Signup</Link>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className
+      ="navbar-nav ms-auto">
+        <li className="nav-item">
+          <Link className
+        ="nav-link active" to="/signup">Signup</Link>
         </li>
-        <li class="nav-item">
+        <li className="nav-item">
           <Link class="nav-link active " to="about">About</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link active" to="products">Products</Link>
+        <li className
+        ="nav-item">
+          <Link className="nav-link active" to="products">Products</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link active " to="pricing">Pricing</Link>
+        <li className="nav-item">
+          <Link className="nav-link active " to="pricing">Pricing</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link active" to="support">Support</Link>
+        <li className="nav-item">
+          <Link className="nav-link active" to="support">Support</Link>
         </li>
       </ul>
     </div>
